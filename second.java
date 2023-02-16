aeec5d674556b44908c8b55d21762ceba58703e1
shashank946 <85671190+shashank946@users.noreply.github.com>
2023-02-16 12:59:22 +0530

    Update and rename second to second.java

--- second.java
+++ second.java
@@ -1,6 +1,6 @@
 public class firstjava
 {
- public static void main(String[]args)
+ public static void main(String[] args)
  {
   System.out.println("hello! welcome to jenkins!Demonstrating freestyle");
  }
